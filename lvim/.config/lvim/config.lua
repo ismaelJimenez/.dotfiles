c9ac99ce566deb0c01b3a622c326478fa86a0ac1
ismael <ismael.jimenez-martinez@bmw.de>
2023-05-03 07:18:19 +0000

    unset format on save

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -7,19 +7,6 @@ reload("user.which-key")
  THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
  `lvim` is the global options object
 ]]
--- general
-lvim.log.level = "info"
-lvim.format_on_save = {
-  enabled = true,
-  pattern = "*.lua",
-  timeout = 1000,
-}
--- to disable icons and use a minimalist setup, uncomment the following
--- lvim.use_icons = false
-
--- -- Change theme settings
--- lvim.colorscheme = "lunar"
-
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
--- lvim/.config/lvim/lua/user/options.lua
+++ lvim/.config/lvim/lua/user/options.lua
@@ -4,3 +4,11 @@ vim.opt.tabstop = 2
 vim.opt.relativenumber = true
 
 vim.keymap.set("i", "jk", "<Esc>")
+
+-- general
+lvim.log.level = "info"
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
+
+-- -- Change theme settings
+-- lvim.colorscheme = "lunar"
