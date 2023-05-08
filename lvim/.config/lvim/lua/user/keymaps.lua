c50ead2221ab9f3cec090de912d238a49299eb64
ismael <ismael.jimenez-martinez@bmw.de>
2023-05-08 12:40:41 +0000

    Add keymap for splits

--- lvim/.config/lvim/lua/user/keymaps.lua
+++ lvim/.config/lvim/lua/user/keymaps.lua
@@ -3,5 +3,8 @@ lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.visual_mode["p"] = '"_dP'
 
+lvim.keys.normal_mode["*"] = { "<cmd>vsplit<cr>", desc = "Vertical Split" }
+lvim.keys.normal_mode["+"] = { "<cmd>split<cr>", desc = "Horizontal Split" }
+
 -- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 -- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
